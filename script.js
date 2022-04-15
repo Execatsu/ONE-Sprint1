0a3ae0adce4bfb6cc12439eb79b9abe7a31a7f3e
Execatsu <cassiusfernando12@gmail.com>
2022-04-15 00:34:34 -0300

    quase tudo pronto

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const input = document.querySelector("main div input")
 const textGenerated = document.querySelector(".genTxt p")
 const genTxt = document.querySelector("#genTxt")
 const noMsg = document.querySelector("#noMsg")
+let displacement = 3;
 
-// input.addEventListener('change', 
-function a() {
+input.addEventListener('change', () => {
     if (input.value === "") {
         btnDecrypt.setAttribute('disabled', true)
         btnEncrypt.setAttribute('disabled', true);
@@ -17,32 +17,57 @@ function a() {
         btnEncrypt.removeAttribute('disabled');
     }
 }
-// );
+);
 
 btnEncrypt.addEventListener('click', () => {
 	let output = "";
-	let displacement = 3;
 	
-	let index = 0;
 	for (let letter of input.value.toLocaleUpperCase()) {
-		output += String.fromCharCode(65 + (((letter.charCodeAt(0) - 65) + displacement) % 26));
-		index += 1;
+        if (letter == " ") {
+            output += " ";
+        }
+		else {
+            output += String.fromCharCode(65 + (((letter.charCodeAt(0) - 65) + displacement) % 26));
+        }
 	}
 	
-    alert(output);
-	textGenerated.value.innerText = output;
+	textGenerated.innerText = output;
 
-    noMsg.style.display = "none"
+    noMsg.style.display = "none";
+    genTxt.style.display = "flex";
 });
 
+btnDecrypt.addEventListener('click', () => {
+	let output = "";
+	
+	for (let letter of input.value.toLocaleUpperCase()) {
+        if (letter == " ") {
+            output += " ";
+        }
+		else {
+            output += String.fromCharCode(decrypt(letter));
+        }
+	}
+	
+	textGenerated.innerText = output;
+    
+    noMsg.style.display = "none";
+    genTxt.style.display = "flex";
+});
 
+function decrypt(letter) {
+    let charc = (letter.charCodeAt(0) - displacement - 65)
+    if (charc != parseInt(charc)) {
+        console.log("if = true; charc >>> " + charc)
+        return charc + 65;
+    } else {
+        console.log("if = false; charc >>> " + 91 + charc); 
+        return (26 + charc + 65);
+    }
+}
 
-
-
-// btnDecrypt.addEventListener('click', () => {
-
-// });
-
-// btnCopy.addEventListener('click', () => {
-
-// });
+btnCopy.addEventListener('click', () => {
+    document.execCommand("copy");
+    alert("O texto é: " + textGenerated.textContent);
+    document.queryCommandValue("copy")
+});
